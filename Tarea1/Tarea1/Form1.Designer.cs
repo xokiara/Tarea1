656fa1702a76b1193c063755d477d39b5ff7ae71
xokiara <kiaramoncada28@gmail.com>
2023-02-03 23:20:06 -0600

    Ejercicio/Tarea 1
    Calcular si el número es par o impar, positivo o negativo

---
+++ Tarea1/Tarea1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Tarea1
+{
+    partial class Ejercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.NumeroLabel = new System.Windows.Forms.Label();
+            this.NumeroTextBox = new System.Windows.Forms.TextBox();
+            this.EjecutarButton = new System.Windows.Forms.Button();
+            this.ErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NumeroLabel
+            // 
+            this.NumeroLabel.AutoSize = true;
+            this.NumeroLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumeroLabel.Location = new System.Drawing.Point(25, 60);
+            this.NumeroLabel.Name = "NumeroLabel";
+            this.NumeroLabel.Size = new System.Drawing.Size(174, 23);
+            this.NumeroLabel.TabIndex = 0;
+            this.NumeroLabel.Text = "Ingrese un número:";
+            // 
+            // NumeroTextBox
+            // 
+            this.NumeroTextBox.Location = new System.Drawing.Point(221, 57);
+            this.NumeroTextBox.Name = "NumeroTextBox";
+            this.NumeroTextBox.Size = new System.Drawing.Size(98, 29);
+            this.NumeroTextBox.TabIndex = 1;
+            // 
+            // EjecutarButton
+            // 
+            this.EjecutarButton.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EjecutarButton.Location = new System.Drawing.Point(221, 123);
+            this.EjecutarButton.Name = "EjecutarButton";
+            this.EjecutarButton.Size = new System.Drawing.Size(98, 35);
+            this.EjecutarButton.TabIndex = 2;
+            this.EjecutarButton.Text = "Ejecutar";
+            this.EjecutarButton.UseVisualStyleBackColor = true;
+            this.EjecutarButton.Click += new System.EventHandler(this.EjecutarButton_Click);
+            // 
+            // ErrorProvider
+            // 
+            this.ErrorProvider.ContainerControl = this;
+            // 
+            // Ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 220);
+            this.Controls.Add(this.EjecutarButton);
+            this.Controls.Add(this.NumeroTextBox);
+            this.Controls.Add(this.NumeroLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Ejercicio1";
+            this.Text = "Ejercicio1";
+            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NumeroLabel;
+        private System.Windows.Forms.TextBox NumeroTextBox;
+        private System.Windows.Forms.Button EjecutarButton;
+        private System.Windows.Forms.ErrorProvider ErrorProvider;
+    }
+}
+
---
+++ Tarea1/Tarea1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea1
+{
+    public partial class Ejercicio1 : Form
+    {
+        public Ejercicio1()
+        {
+            InitializeComponent();
+        }
+
+        //Variables globales
+        decimal resultado = 0;
+        decimal resultado2 = 0;
+
+        private void EjecutarButton_Click(object sender, EventArgs e)
+        {
+
+            //Validación del Text Box
+            if (NumeroTextBox.Text == "") 
+            {
+                ErrorProvider.SetError(NumeroTextBox, "Ingrese un valor");
+                return;
+            }
+
+            ErrorProvider.Clear();
+
+            //Llamado de función Par o impar
+            decimal resultado = ParOImpar(Convert.ToDecimal(NumeroTextBox.Text));
+
+            //Llamado de función Positivo o Negativo
+            decimal resultado2 = PositivoONegativo(Convert.ToDecimal(NumeroTextBox.Text));
+        }
+
+        //Función Par o Impar
+        private decimal ParOImpar(decimal num) 
+        {
+            if (num % 2 == 0) 
+            {
+                MessageBox.Show("El número es Par", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("El número es Impar", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            return resultado;
+        
+        }
+
+        //Función Positivo o Negativo
+        private decimal PositivoONegativo(decimal num) 
+        { 
+            if (num > 0)
+            {
+                MessageBox.Show("El número es Positivo", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (num < 0) 
+            {
+                MessageBox.Show("El número es Negativo", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            return resultado2;
+
+        }
+    }
+}
